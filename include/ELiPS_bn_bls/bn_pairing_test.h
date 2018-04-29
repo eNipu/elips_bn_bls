a8792426b696b640a2a674a6a2af3d14c6f9a046
Md Al-Amin Khandaker <xion.nipu@gmail.com>
2018-04-29 16:28:14 +0900

    Added copyright

--- include/ELiPS_bn_bls/bn_pairing_test.h
+++ include/ELiPS_bn_bls/bn_pairing_test.h
@@ -3,8 +3,28 @@
 //  elips_refactoring
 //
 //  Created by Khandaker Md. Al-Amin on 2/2/18.
-//  Copyright © 2018 ISec Lab. All rights reserved.
-//
+
+/*
+ * ELiPS is an Efficient Library for Pairing-based Systems
+ * Copyright (C) 2008-2018 ELiPS Authors. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * This file is part of ELiPS Project. ELiPS is legal property of its
+ * developers, whose names are not listed here. 
+ *
+ * ELiPS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * ELiPS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ELiPS. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef bn_pairing_test_h
 #define bn_pairing_test_h
